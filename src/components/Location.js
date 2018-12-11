01580cf44ffc027bbb2ac121aa78fd5b6d1bef72
Armando Rojas (e999127) <ajrojas@cuprum.cl>
2018-12-11 12:08:17 -0300

    Indentando el código

--- src/components/Location.js
+++ src/components/Location.js
@@ -5,7 +5,12 @@ const Location = ({ city }) => {
     //const { city } = props;
 
     return (
-        <div><h1>{city}</h1></div>);
+        <div>
+            <h1>
+                {city}
+            </h1>
+        </div>
+    );
 };
 
 export default Location;
