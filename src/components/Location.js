b025bfebe5914d358e3609d62428bf9778cc83ed
Armando Rojas (e999127) <ajrojas@cuprum.cl>
2018-12-11 11:05:23 -0300

    Agregó al 'arrow function' un parametro ('props' por convención)

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const Location = () => (
-    <div><h1>Buenos Aires</h1></div>
-);
+const Location = (props) => {
+    console.log(props);
+    debugger;
+    return(
+        <div><h1>Buenos Aires</h1></div>);
+};
 
 export default Location;
