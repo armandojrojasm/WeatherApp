fd99c2dc2f40f472ebe517980661585b35803a1d
Armando Rojas (e999127) <ajrojas@cuprum.cl>
2018-12-14 12:40:00 -0300

    Se agregan estilos a WeatherLocation

--- src/components/WeatherLocation/Location.js
+++ src/components/WeatherLocation/Location.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
 
 const Location = ({ city }) => {
     //destructuring
     //const { city } = props;
 
     return (
-        <div>
+        <div className="locationCont">
             <h1>
                 {city}
             </h1>
