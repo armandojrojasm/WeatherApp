cf5463f353091b2104298c914bd2d4182ec926d0
Armando Rojas (e999127) <ajrojas@cuprum.cl>
2018-12-17 15:21:41 -0300

    Doble 'destructuring'
    Se elimina la constante

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTempature from './WeatherTemperature';
-import {
-    SUN,
-} from './../../../constants/weathers';
 import './styles.css';
 
-const WeatherData = ({ data }) => {
-    const { temperature, weatherState, humidity, wind } = data;
-
-    return (<div className="weatherDataCont">
+//doble destructuring
+const WeatherData = ({ data: { temperature, weatherState, humidity, wind } }) => (
+    <div className="weatherDataCont">
         <WeatherTempature 
             temperature={temperature} 
             weatherState={weatherState}>
         </WeatherTempature>
         <WeatherExtraInfo humidity={humidity} wind={wind}></WeatherExtraInfo>
-    </div>);
-};
+    </div>
+);
 
 export default WeatherData;
