ecdbc61a8c9d4093d7d5bd670572fcdd8d0314ef
Armando Rojas (e999127) <ajrojas@cuprum.cl>
2018-12-26 16:31:42 -0300

    Se agrega query string 'units=metric' para obtener valores en sistema metrico

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -11,7 +11,7 @@ const location = "Buenos Aires,ar";
 const api_key = "96b8e87ae43c0ed78d9b24fac11fed1c";
 const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
 
-const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}&units=metric`;
 
 const data = {
     temperature: 27,
