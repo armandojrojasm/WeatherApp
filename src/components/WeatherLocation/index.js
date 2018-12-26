69c64faeb958a191381d2c2f769e6ac4e8be3954
Armando Rojas (e999127) <ajrojas@cuprum.cl>
2018-12-26 14:39:18 -0300

    Se agregan constantes y se hace llamado al 'fetch' con el url del API

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -7,6 +7,12 @@ import {
     WINDY
 } from './../../constants/weathers';
 
+const location = "Buenos Aires,ar";
+const api_key = "96b8e87ae43c0ed78d9b24fac11fed1c";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
 const data = {
     temperature: 27,
     weatherState: SUN,
@@ -50,6 +56,8 @@ class WeatherLocation extends Component {
     }      
 
     handleUpdateClick = () => {
+        fetch(api_weather);
+
         console.log("actualizado");
         
         //esto no sirve, el unico sitio donde puedo hacer es 'this.state' es en constructor
