2f6cb75c233847b0e249cc987839fcd90cd8c468
Medusoft <rojas.moreira@gmail.com>
2019-01-07 16:50:24 -0300

    El 'key' debe ser algo que no cambie, como el valor 'city'. El 'index' puede cambiar si en el arreglo se mete un elemento por delante de otro, por lo que de ahí en adelante los indices no serían los mismos y React volvería a renderizar esos elementos

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -3,7 +3,8 @@ import WeatherLocation from './WeatherLocation';
 import PropTypes from 'prop-types';
 
 const strToComponents = (cities) => (
-    cities.map((city, index) => ( <WeatherLocation key={index} city={city} /> ))
+    //cities.map((city, index) => ( <WeatherLocation key={index} city={city} /> ))
+    cities.map(city => ( <WeatherLocation key={city} city={city} /> ))
 );
 
 const LocationList = ({ cities }) => (
