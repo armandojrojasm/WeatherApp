428a466d5922f1c89a8dda2695903fe6ae1e90e9
Medusoft <rojas.moreira@gmail.com>
2019-01-09 16:21:17 -0300

    Uso de 'react-flexbox-grid'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import LocationList from './components/LocationList';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+//import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 const cities = [
   "Lima,pe",
@@ -18,12 +20,59 @@ class App extends Component {
     console.log(`(App) handleSelectedLocation: ${city}`);
   };
 
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <LocationList cities={cities}
+  //       onSelectedLocation={this.handleSelectedLocation}></LocationList>
+  //     </div>
+  //   );
+  // }
+
   render() {
     return (
+      
+
+/*       <div>
+        <Grid>
+          <Row>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='red'>&nbsp;</div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='green'>&nbsp;</div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='blue'>&nbsp;</div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='yellow'>&nbsp;</div>
+            </Col>                                    
+          </Row>
+        </Grid>
+      </div> */
       <div className="App">
-        <LocationList cities={cities}
-        onSelectedLocation={this.handleSelectedLocation}></LocationList>
+        <LocationList cities={cities} 
+          onSelectedLocation={this.handleSelectedLocation}></LocationList>
+        <Grid fluid>
+          <Row>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='red'>&nbsp;</div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='green'>&nbsp;</div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='blue'>&nbsp;</div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='yellow'>&nbsp;</div>
+            </Col>                                    
+          </Row>
+        </Grid>
       </div>
+
+
     );
   }
 }
