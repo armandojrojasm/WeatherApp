56d51c24a39ca08dce269fb173c6284ea11a45a4
Medusoft <rojas.moreira@gmail.com>
2019-01-10 17:38:01 -0300

    Se hace uso de 'MediaQuery'

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import WeatherLocation from './WeatherLocation';
 import PropTypes from 'prop-types';
+import './styles.css';
 
 const LocationList = ({ cities, onSelectedLocation }) => {
 
@@ -18,7 +19,7 @@ const LocationList = ({ cities, onSelectedLocation }) => {
     );
 
     return(
-        <div>
+        <div className="locationList">
             { strToComponents(cities) }
         </div>
     );
